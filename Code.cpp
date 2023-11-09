d24a65e725ab583e48c4bc69864c4e9478cdb1df
Kfro029 <91137416+Kfro029@users.noreply.github.com>
2023-11-09 22:24:28 +0300

    Add files via upload

--- Code.cpp
+++ Code.cpp
@@ -21,15 +21,15 @@ DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N>& po
 	for (unsigned int i = 0; i < N+1; i++) {
 		A[i][N+1] = 0;
 		A[i][0] = 0;
+		A[0][i] = 1;
 	};
 	A[1][N+1] = 1;
-	A[0][0] = 1;
 	
 
 	//Заполняем остальные столбцы A
-	for (unsigned int i = 0; i < N+1; i++) {
+	for (unsigned int i = 1; i < N+1; i++) {
 		for (unsigned int j = 1; j < N+1; j++) {
-			A[i][j] = pow(points[j-1], i);
+			A[i][j] = A[i-1][j] * points[j-1];
 			
 			
 		};
@@ -84,14 +84,6 @@ DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N>& po
 				A[j][k] = A[j][k] - A[i][k] * A[j][i];
 			}
 		}
-		
-		for (unsigned int l = 0; l < N + 1; l++) {
-		for (unsigned int y = 0; y < N + 2; y++) {
-			std::cout << A[l][y] << " ";
-		}
-		std::cout << std::endl;
-	};
-	std::cout << std::endl;
 
 	};
 	
@@ -125,24 +117,18 @@ int main()
 
 	//Вывод на экран коэффициентов
 	DerivativeCoef<double, N-1> A3 = calcDerivativeCoef(points3);
-	std::cout << "Central coef " << A3.centralCoef << std::endl;
-	std::cout << "Other coefs" << " ";
-	for (unsigned int i = 0; i < N - 1; i++) {
-		std::cout << A3.otherCoefs[i] << " ";
-	}
-
-
+	
 
 	//Запись в файл 
-	for (int i = 0; i > -16; i--) {
+	for (int i = 0; i > -50; i--) {
 		//sum - это D(f)*h (в обозначениях с семинара) 
 		double sum = A3.centralCoef* exp(x_0);
 		for (int j = 0; j < N-1; j++) {
-			sum += A3.otherCoefs[j] * exp(x_0 + points3[j] * pow(10., i));
+			sum += A3.otherCoefs[j] * exp(x_0 + points3[j] * pow(2., i));
 		}
-		sum = sum / pow(10, i); // теперь sum это D(f)
+		sum = sum / pow(2, i); // теперь sum это D(f)
 		double fail = abs(sum - exp(x_0));
-		fout << pow(10., i) << " " << fail << std::endl;
+		fout << pow(2., i) << " " << fail << std::endl;
 
 	}
 
